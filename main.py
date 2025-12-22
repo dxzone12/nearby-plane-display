ef7503fb2e355953f4946d2707bca5c3edaef905
Ben Morris <dxzone12@users.noreply.github.com>
2025-12-22 21:49:07 +1100

    add arg parsing and basic call

--- main.py
+++ main.py
@@ -1,5 +1,27 @@
+
+import argparse
+import requests
+
+
+def parse_args():
+    parser = argparse.ArgumentParser(prog="Nearby Plane Display",
+                                     description="Displays nearby planes based on given coordinates.")
+    
+    parser.add_argument("latitude", type=float, help="Latitude of the center point")
+    parser.add_argument("longitude", type=float, help="Longitude of the center point")
+    parser.add_argument("-h", "--hostname", type=str, default="localhost", help="Hostname of the readsb api endpoint (default: localhost)")
+    parser.add_argument("-p", "--port", type=int, default=54321, help="Port of the readsb api endpoint (default: 54321)")
+    parser.add_argument("-r", "--radius", type=float, default=150, help="Radius of the circle in kilometers (default: 150 nmi)")
+    
+    return parser.parse_args()
+
 def main():
-    print("Hello from nearby-plane-display!")
+    args = parse_args()
+
+    url = f"http://{args.hostname}:{args.port}/?circle={args.latitude},{args.longitude},{args.radius}&filter_with_pos"
+    
+    resp = requests.get(url)
+    print(resp.json())
 
 
 if __name__ == "__main__":
