87e4f21877a8321a997826d993479b50b8011306
Ben Morris <dxzone12@users.noreply.github.com>
2025-12-23 23:55:43 +1100

    Add a basic ui

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-
 import argparse
 import requests
+import tkinter as tk
+import tkinter.ttk as ttk
+from DetailsDisplay import DetailsDisplay
 
 
 def parse_args():
@@ -20,8 +22,32 @@ def main():
 
     url = f"http://{args.hostname}:{args.port}/?circle={args.latitude},{args.longitude},{args.radius}&filter_with_pos"
     
-    resp = requests.get(url)
-    print(resp.json())
+    window = tk.Tk()
+    window.title("Nearby Plane Display")
+    # window.attributes("-fullscreen", True)
+    
+    window.columnconfigure(0, weight=1, minsize=100)
+    window.columnconfigure(1, weight=2, minsize=100)
+    window.rowconfigure(0, weight=1, minsize=100)
+
+    left_frame = ttk.Frame(master=window)
+    left_frame.grid(row=0, column=0, sticky="nsew")
+
+    right_frame = ttk.Frame(master=window)
+    right_frame.grid(row=0, column=1, sticky="nsew")
+
+    details_frame = DetailsDisplay(right_frame)
+
+    left_frame.columnconfigure(0, weight=1)
+    left_frame.rowconfigure(0, weight=1)
+    image_label = ttk.Label(master=left_frame, text="no image found", anchor=tk.CENTER, borderwidth=1, relief=tk.SUNKEN)
+    image_label.grid(row=0, column=0, sticky="nsew")
+
+    window.mainloop()
+
+    # resp = requests.get(url)
+    # response_json = resp.json()
+    # print(response_json)
 
 
 if __name__ == "__main__":
