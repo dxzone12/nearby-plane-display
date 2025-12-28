46432be2340fc552a47d78057ad56000782c5cec
Ben Morris <dxzone12@users.noreply.github.com>
2025-12-28 15:34:28 +1100

    Attempt to get fonts working on raspi

--- main.py
+++ main.py
@@ -81,7 +81,8 @@ def main():
     details_frame = PlaneDetailsFrame(right_frame)
 
     style = ttk.Style(window)
-    style.configure("TLabel", font=("TKDefaultFont", 40))
+    style.configure("TLabel", font=("TKDefaultFont", 20))
+    style.configure("NPD.Label", font=("TKDefaultFont", 40, "bold"))
 
     window.after(0, get_and_update_plane_details, url, details_frame)
 
