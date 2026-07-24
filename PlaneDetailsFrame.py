2408aa1a142d0329634433844a7590c0cd90a1c3
Ben Morris <dxzone12@users.noreply.github.com>
2026-07-25 00:12:18 +1000

    Set the image into the label using raw size for now to see results

--- PlaneDetailsFrame.py
+++ PlaneDetailsFrame.py
@@ -1,6 +1,7 @@
 import tkinter as tk
 import tkinter.ttk as ttk
 from PlaneDetails import PlaneDetails
+from PIL import Image, ImageTk
 
 class PlaneDetailsFrame:
     def __init__(self, title_frame: ttk.Frame, bottom_frame: ttk.Frame, image_frame: ttk.Frame):
@@ -22,6 +23,8 @@ class PlaneDetailsFrame:
         self._image_frame = image_frame
         self._image_label = ttk.Label(master=self._image_frame, text="no image found", anchor=tk.CENTER, borderwidth=1, relief=tk.SUNKEN)
         self._image_label.grid(row=0, column=0, sticky=tk.NSEW)
+        self._image_file_name = ""
+        self._resized_image: ImageTk.PhotoImage | None = None
 
         # Initialise title frame
         ttk.Label(master=self._title_frame, text="Callsign:", anchor=tk.W).grid(row=0, column=0, sticky=tk.NSEW, padx=3)
@@ -88,6 +91,24 @@ class PlaneDetailsFrame:
         self._ground_speed_label["text"] = f"{plane_details.ground_speed} kt"
         self._distance_from_center_label["text"] = f"{plane_details.distance_from_center} nm"
         self._last_seen_label["text"] = f"{plane_details.plane_seen_ago} sec (pos: {plane_details.pos_received_ago} sec)"
+
+        image_to_set = self.get_image(plane_details.image_file_name)
+        self._image_label.config(image=image_to_set)
+
+    def get_image(self, file_name: str) -> ImageTk.PhotoImage | str:
+        if file_name == "":
+            self._image_file_name = ""
+            self._resized_image = None
+            return ""
+        
+        if file_name == self._image_file_name:
+            return self._resized_image if self._resized_image is not None else ""
+        
+        raw_image = Image.open(file_name)
+        self._resized_image = ImageTk.PhotoImage(raw_image)
+
+        self._image_file_name = file_name
+        return self._resized_image
 
     def altitude_change_indicator(self, altitude_rate: int) -> str:
         if altitude_rate > 0:
